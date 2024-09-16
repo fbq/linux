601a505996dc18c1ed85588340e65d71414e5447
Lyude Paul <lyude@redhat.com>
2024-09-16 17:28:05 -0400

    rust: sync: Introduce lock::Backend::Context
    Now that we've introduced an `InterruptDisabled` token for marking
    contexts in which IRQs are disabled, we can have a way to avoid
    `SpinLockIrq` disabling interrupts if the interrupts have already been
    disabled. Basically, a `SpinLockIrq` should work like a `SpinLock` if
    interrupts are disabled. So a function:
    
    	(&'a SpinLockIrq, &'a InterruptDisabled) -> Guard<'a, .., SpinLockBackend>
    
    makes senses. Note that due to `Guard` and `InterruptDisabled` has the
    same lifetime, interrupts cannot be enabled whiel the Guard exists.
    
    Add a `lock_with()` interface for `Lock`, and an associate type of
    `Backend` to describe the context.
    
    [Boqun: Change the interface a lot, now `SpinLockIrq` can use the
    `lock()` function, but it always disable the interrupts, reuse the
    `lock_with()` method to provide a way for locking if interrupts are
    already disabled. `lock_with()` implementation will be added later.]
    
    Signed-off-by: Lyude Paul <lyude@redhat.com>
    Co-Developed-by: Boqun Feng <boqun.feng@gmail.com>
    Signed-off-by: Boqun Feng <boqun.feng@gmail.com>

--- rust/kernel/sync/lock/spinlock.rs
+++ rust/kernel/sync/lock/spinlock.rs
@@ -3,6 +3,7 @@
 //! A kernel spinlock.
 //!
 //! This module allows Rust code to use the kernel's `spinlock_t`.
+use crate::interrupt::InterruptDisabled;
 
 /// Creates a [`SpinLock`] initialiser with the given name and a newly-created lock class.
 ///
@@ -100,6 +101,7 @@ pub type SpinLockGuard<'a, T> = super::Guard<'a, T, SpinLockBackend>;
 unsafe impl super::Backend for SpinLockBackend {
     type State = bindings::spinlock_t;
     type GuardState = ();
+    type Context<'a> = ();
 
     unsafe fn init(
         ptr: *mut Self::State,
@@ -209,6 +211,7 @@ pub struct SpinLockIrqBackend;
 unsafe impl super::Backend for SpinLockIrqBackend {
     type State = bindings::spinlock_t;
     type GuardState = ();
+    type Context<'a> = &'a InterruptDisabled;
 
     unsafe fn init(
         ptr: *mut Self::State,
