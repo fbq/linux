cce75cde65f0b883291c50512e550fd336de83ff
Boqun Feng <boqun.feng@gmail.com>
2024-09-18 01:39:37 -0700

    WIP: rust: sync: atomic: Add Atomic<u64>
    Signed-off-by: Boqun Feng <boqun.feng@gmail.com>

--- rust/kernel/sync/atomic.rs
+++ rust/kernel/sync/atomic.rs
@@ -19,3 +19,45 @@
 pub mod generic;
 pub mod ops;
 pub mod ordering;
+
+/// ```rust
+/// use kernel::sync::atomic::generic::*;
+/// use kernel::sync::atomic::ordering::*;
+///
+/// let x = Atomic::new(42u64);
+///
+/// assert_eq!(42, x.read(Relaxed));
+/// ```
+// SAFETY: `u64` and `i64` has the same size and alignment.
+unsafe impl generic::AllowAtomic for u64 {
+    type Repr = i64;
+
+    fn into_repr(self) -> Self::Repr {
+        self as _
+    }
+
+    fn from_repr(repr: Self::Repr) -> Self {
+        repr as _
+    }
+}
+
+/// ```rust
+/// use kernel::sync::atomic::generic::*;
+/// use kernel::sync::atomic::ordering::*;
+///
+/// let x = Atomic::new(42u64);
+///
+/// assert_eq!(42, x.fetch_add(12, Full));
+/// assert_eq!(54, x.read(Relaxed));
+///
+/// x.add(13, Relaxed);
+///
+/// assert_eq!(67, x.read(Relaxed));
+/// ```
+impl generic::AllowAtomicArithmetic for u64 {
+    type Delta = u64;
+
+    fn delta_into_repr(d: Self::Delta) -> Self::Repr {
+        d as _
+    }
+}
