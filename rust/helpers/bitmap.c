7144898a2e8fd6f4d8a2841f7f229944d6fef55e
Boqun Feng <boqun.feng@gmail.com>
2025-04-24 10:37:42 -0700

    rust: bit{map,ops}: Equip Bitmap with RUST_INLINE_HELPERS
    Signed-off-by: Boqun Feng <boqun.feng@gmail.com>

--- rust/helpers/bitmap.c
+++ rust/helpers/bitmap.c
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: GPL-2.0
 
 #include <linux/bitmap.h>
+#include "helpers.h"
 
-void rust_helper_bitmap_copy_and_extend(unsigned long *to, const unsigned long *from,
+__rust_helper void rust_helper_bitmap_copy_and_extend(unsigned long *to, const unsigned long *from,
 		unsigned int count, unsigned int size)
 {
 	bitmap_copy_and_extend(to, from, count, size);
